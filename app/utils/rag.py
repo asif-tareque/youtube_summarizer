c5ee866520bfaf67f4965ed77120dee411f92dac
asif-tareque <gmasiftareque@gmail.com>
2026-04-11 12:35:25 +1000

    fix:faiss index logic

--- app/utils/rag.py
+++ app/utils/rag.py
@@ -14,16 +14,19 @@ def chunk_transcript(processed_transcript, chunk_size=200, chunk_overlap=20):
     chunks = text_splitter.split_text(processed_transcript)
     return chunks
 
-def create_embeddings(chunks, embedding_model=None):
-    # Create embeddings for each chunk using the provided embedding model
-    if embedding_model is None:
-        embedding_model = embeddings = OpenAIEmbeddings(
-    model="text-embedding-3-small"  
-)
-    embeddings = embedding_model.embed_documents(chunks)
-    return embeddings
-
 def create_faiss_index(chunks, embedding_model):
    
     # Use the FAISS library to create an index from the provided text chunks
     return faiss.from_texts(chunks, embedding_model)
+
+def create_faiss_index(chunks, embedding_model=None):
+    # Create embeddings for each chunk using the provided embedding model
+    if embedding_model is None:
+        embedding_model = OpenAIEmbeddings(
+            model="text-embedding-3-small"
+        )
+    faiss_index = create_faiss_index(chunks, embedding_model)
+    return faiss_index
+
+
+
